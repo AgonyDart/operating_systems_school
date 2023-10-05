c52ca0003ffc8bff203842a0f12e2984129bab3c
Angel Duarte <adavid.tinocod@gmail.com>
2023-10-05 14:30:56 -0600

    [UPDATE] 2.4_finishied?

--- 002_process/parallel.c
+++ 002_process/parallel.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 int array_size;
+void wait();
 int subprocesses;
 
 // A function to compare two integers for qsort
@@ -48,6 +49,9 @@ void order_numbers(int *arr, int n)
 
 int main()
 {
+  clock_t start, end;
+  double execution_time;
+  start = clock();
   printf("Enter the length of the array: ");
   scanf("%d", &array_size);
 
@@ -65,7 +69,7 @@ int main()
     srand(time(0));
     for (int i = 0; i < array_size; i++)
     {
-      p_original_array[i] = rand() % 1024 + 1;
+      p_original_array[i] = rand() % 1048576 + 1;
     }
     printf("Array elements are: [");
     for (int i = 0; i < array_size; i++)
@@ -77,10 +81,22 @@ int main()
     scanf("%d", &subprocesses);
 
     int chunk_size = array_size / subprocesses;
-    int remainder = array_size % subprocesses;
     // printf("Chunk size: %d\n", chunk_size);
+    int remainder = array_size % subprocesses;
+    int pipes[subprocesses][2];
+
+    // Create a pipe for each subprocess
     for (int i = 0; i < subprocesses; i++)
     {
+      if (pipe(pipes[i]) == -1)
+      {
+        perror("Pipe failed");
+      }
+    }
+
+    for (int i = 0; i < subprocesses; i++)
+    {
+
       pid_t child = fork();
       if (child < 0)
       {
@@ -94,6 +110,8 @@ int main()
         {
           chunk_size += remainder;
         }
+        // Close the read end of the pipe
+        close(pipes[i][0]);
         int *p_chunk_array = (int *)malloc(chunk_size * sizeof(int));
         if (p_chunk_array == NULL)
         {
@@ -104,7 +122,7 @@ int main()
         {
           int k = 0;
 
-          printf("\nSubprocess %d", i);
+          printf(" -- -- -- -- --\nSubprocess %d", i);
           printf(", My array elements are: [");
 
           for (int j = i * chunk_size; j < (i + 1) * chunk_size; j++)
@@ -124,10 +142,55 @@ int main()
           }
 
           printf("]\n");
+          write(pipes[i][1], p_chunk_array, chunk_size * sizeof(int));
+          // Close the write end of the pipe
+          close(pipes[i][1]);
           exit(0);
         }
       }
     }
-    return 0;
+    // Parent process
+    int *p_merged_array = (int *)malloc(array_size * sizeof(int));
+    if (p_merged_array == NULL)
+    {
+      perror("Memory allocation failed");
+    }
+    else
+    {
+      int merge_index = 0;
+      for (int i = 0; i < subprocesses; i++)
+      {
+        wait(0);
+        close(pipes[i][1]);
+        int *p_chunk_array = (int *)malloc(chunk_size * sizeof(int));
+        if (p_chunk_array == NULL)
+        {
+          perror("Memory allocation failed");
+        }
+        else
+        {
+          read(pipes[i][0], p_chunk_array, chunk_size * sizeof(int));
+          close(pipes[i][0]);
+          for (int j = 0; j < chunk_size; j++)
+          {
+            p_merged_array[merge_index] = p_chunk_array[j];
+            merge_index++;
+          }
+          free(p_chunk_array);
+        }
+      }
+      order_numbers(p_merged_array, array_size);
+      end = clock();
+      printf("\nMerged ordered array elements are: [");
+      for (int i = 0; i < array_size; i++)
+      {
+        printf("%d ", p_merged_array[i]);
+      }
+      printf("]\n");
+      free(p_merged_array);
+      execution_time = (double)(end - start) / CLOCKS_PER_SEC;
+      printf("===========================================\nThe program took %f seconds to execute\n", execution_time);
+    }
   }
+  return 0;
 }
