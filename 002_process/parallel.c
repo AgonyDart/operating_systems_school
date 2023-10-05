7108ad2834da2587b18e9af6db9810c13c786d71
Angel Duarte <adavid.tinocod@gmail.com>
2023-10-05 05:04:59 -0600

    [UPDATE] order algorithm

--- 002_process/parallel.c
+++ 002_process/parallel.c
@@ -8,6 +8,45 @@ int *ptr;
 int array_size;
 int x;
 
+// A function to compare two integers for qsort
+int compare(const void *a, const void *b)
+{
+  int x = *(int *)a;
+  int y = *(int *)b;
+
+  // If both are even, compare in descending order
+  if (x % 2 == 0 && y % 2 == 0)
+  {
+    return y - x;
+  }
+
+  // If both are odd, compare in ascending order
+  if (x % 2 == 1 && y % 2 == 1)
+  {
+    return x - y;
+  }
+
+  // If x is even and y is odd, x comes first
+  if (x % 2 == 0 && y % 2 == 1)
+  {
+    return -1;
+  }
+
+  // If x is odd and y is even, y comes first
+  if (x % 2 == 1 && y % 2 == 0)
+  {
+    return 1;
+  }
+
+  // This should never happen
+  return 0;
+}
+
+void order_numbers(int *arr, int n)
+{
+  qsort(arr, n, sizeof(int), compare);
+}
+
 int main()
 {
   printf("Enter the length of the array: ");
@@ -52,19 +91,31 @@ int main()
       else if (pid == 0)
       {
         // Child process
+        int *ptr_temp = (int *)malloc(chunk_size * sizeof(int));
+        int k = 0;
+
         printf("\nSubprocess %d", i);
         printf(", My array elements are: [");
         for (int j = i * chunk_size; j < (i + 1) * chunk_size; j++)
         {
-          printf("%d ", ptr[j]);
+          ptr_temp[k] = ptr[j];
+          printf("%d ", ptr_temp[k]);
+          k++;
         }
-        printf("]");
+        int aux = 0;
+        int *p = &aux;
+        int ptr_temp_size = sizeof(ptr_temp_size) / sizeof(ptr_temp_size[p]);
+        order_numbers(ptr_temp, ptr_temp_size);
+        printf("]\nMy ordered array elements are: [");
+        for (int j = 0; j < chunk_size; j++)
+        {
+          printf("%d ", ptr_temp[j]);
+        }
+
+        printf("]\n");
         exit(0);
       }
     }
-
-    // avoid the '%' sign in the printf statement
-    printf("\n");
   }
   return 0;
 }
--- 002_process/process_tree.c
+++ 002_process/process_tree.c
@@ -3,88 +3,51 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-<<<<<<< HEAD
-// funcion que crea n subprocesos
-void create_children(int n) {
-  pid_t pid;
-  for (int i = 0; i < n; i++) {
-    pid = fork();
-    if (pid == 0) {
-      // subproceso
-      printf("Soy el subproceso %d y mi PID es %d , y mi padre tiene el PID %d\n", i + 1, getpid(), getppid());
-      exit(0);
-    } else if (pid > 0) {
-      // padre
-      wait(NULL);
-    } else {
-      // Fork fail
-      perror("fork");
-      exit(1);
-=======
 void wait();
 
 void crearArbol(int nivel, int maxNiveles)
 {
-    if (nivel == maxNiveles)
+  if (nivel == maxNiveles)
+  {
+    return; // Caso base: No se crean más hijos en el nivel dado
+  }
+
+  for (int i = 0; i <= nivel; i++)
+  {
+    pid_t child = fork();
+
+    if (child < 0)
     {
-        return; // Caso base: No se crean más hijos en el nivel dado
+      fprintf(stderr, "Error al crear el subproceso en el nivel %d\n", nivel);
+      exit(1);
     }
-
-    for (int i = 0; i <= nivel; i++)
+    else if (child == 0)
     {
-        pid_t child = fork();
-
-        if (child < 0)
-        {
-            fprintf(stderr, "Error al crear el subproceso en el nivel %d\n", nivel);
-            exit(1);
-        }
-        else if (child == 0)
-        {
-            // Código ejecutado por el subproceso hijo
-            printf("Soy el subproceso en el nivel %d, mi PID es %d\n", nivel, getpid());
-            // Realiza tareas específicas para el subproceso hijo
-            crearArbol(nivel + 1, maxNiveles); // Llama recursivamente para crear más hijos en el siguiente nivel
-            exit(0);                           // Importante: asegúrate de que los subprocesos hijos terminen correctamente
-        }
->>>>>>> c0736816ef18ba5a6519958616efaa32cbfcca28
+      // Código ejecutado por el subproceso hijo
+      printf("Soy el subproceso en el nivel %d, mi PID es %d\n", nivel, getpid());
+      // Realiza tareas específicas para el subproceso hijo
+      crearArbol(nivel + 1, maxNiveles); // Llama recursivamente para crear más hijos en el siguiente nivel
+      exit(0);                           // Importante: asegúrate de que los subprocesos hijos terminen correctamente
     }
   }
 }
+}
 
-<<<<<<< HEAD
-int main() {
-  pid_t pid;
-  int n = 3; // Numero de hijos del proceso root
-  printf("proceso ROOT con PID  %d\n", getpid());
-  for (int i = 0; i < n; i++) {
-    pid = fork();
-    if (pid == 0) {
-      printf("Soy el subproceso %d y mi PID es %d , y mi padre tiene el PID %d, [ROOT]\n", i + 1, getpid(), getppid());
-      create_children(i + 1); // Crea i + 1 subprocesos basados en el index
-      exit(0);
-    } else if (pid > 0) {
-      wait(NULL); 
-    } else {
-      perror("fork");
-      exit(1);
-=======
 int main()
 {
-    int n = 3; // Cambia n al número deseado de niveles del árbol
+  int n = 3; // Cambia n al número deseado de niveles del árbol
 
-    printf("Soy el proceso raíz, mi PID es %d\n", getpid());
+  printf("Soy el proceso raíz, mi PID es %d\n", getpid());
 
-    crearArbol(0, n); // Llama a la función para crear el árbol de procesos
+  crearArbol(0, n); // Llama a la función para crear el árbol de procesos
 
-    // El proceso raíz puede realizar tareas específicas aquí
+  // El proceso raíz puede realizar tareas específicas aquí
 
-    // Esperar a que todos los subprocesos hijos terminen
-    for (int i = 0; i < n; i++)
-    {
-        wait(NULL);
->>>>>>> c0736816ef18ba5a6519958616efaa32cbfcca28
-    }
+  // Esperar a que todos los subprocesos hijos terminen
+  for (int i = 0; i < n; i++)
+  {
+    wait(NULL);
   }
-  return 0;
+}
+return 0;
 }
