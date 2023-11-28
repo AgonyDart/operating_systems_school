f8dfb3b424509e667cdf24d0725e13920db4cdd2
Angel Duarte <85385463+AgonyDart@users.noreply.github.com>
2023-11-28 08:26:31 -0600

    [UPDATE] memory.py WORKING

--- 003_memory_access/memory.py
+++ 003_memory_access/memory.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import collections as col
+import copy
 
 class Node:
     def __init__(self, base, free):
@@ -61,34 +62,6 @@ class LinkedList:
             prev.next = current.next
 
     # sorting methods
-    def sort_ascending(self):
-        if self.head is None:
-            return
-
-        swapped = True
-        while swapped:
-            swapped = False
-            node = self.head
-            while node.next:
-                if node.free > node.next.free:
-                    node.free, node.next.free = node.next.free, node.free #swap
-                    swapped = True
-                node = node.next
-
-    def sort_descending(self):
-        if self.head is None:
-            return
-
-        swapped = True
-        while swapped:
-            swapped = False
-            node = self.head
-            while node.next:
-                if node.free < node.next.free:
-                    node.free, node.next.free = node.next.free, node.free #swap
-                    swapped = True
-                node = node.next
-
     def sort_by_base(self):
         if self.head is None:
             return
@@ -105,7 +78,7 @@ class LinkedList:
                 current = current.next
 
     # merge free spaces
-    def merge(self):
+    def merge(self, method):
         self.sort_by_base()
         current = self.head 
         while current is not None: # for each node
@@ -117,11 +90,22 @@ class LinkedList:
                 else:
                     runner = runner.next
             current = current.next
-    
+
+        # eliminated code cos it were best to order in the allocation methods :p
+        
+        # if method == 'best':
+        #     self.sort_ascending()
+        # elif method == 'worst':
+        #     self.sort_descending()
+        # self.print_list()
+
     # first fit allocation
-    def first_fit_allocation(self, process_list, processes_exec, processes_waiting, ram, system_len, memory):
-        event_counter = 0 # number of events
-        for process in process_list:
+    def first_fit_allocation(self, process_list, ram, system_len, memory):
+        processes_exec = []
+        processes_waiting = []
+        event_counter = 0 
+        process_list_copy = copy.deepcopy(process_list) 
+        for process in process_list_copy:
             current = self.head
             while current: # Iterate over all nodes
                 if current.free >= process.size: # if there is enough space
@@ -144,7 +128,7 @@ class LinkedList:
                 print(f"----------\nProceso {process.id} terminado.\n----------")
                 # Add a new node to represent the free space
                 self.push_free(process.dir, process.size)
-                self.merge()
+                self.merge(method='first')
                 event_counter += 1
                 memory.print_list()
                 make_bar(ram, system_len, processes_exec, memory, method='Primer ajuste')
@@ -171,6 +155,135 @@ class LinkedList:
         print(f"-------\nEventos: {event_counter}")
         return event_counter
     
+    # best fit allocation
+    def best_fit_allocation(self, process_list, ram, system_len, memory):
+        processes_exec = []
+        processes_waiting = []
+        event_counter = 0 # number of events
+        process_list_copy = copy.deepcopy(process_list) # make a copy of the process list
+        for process in process_list_copy:
+            memory.print_list()
+            current = self.head
+            best_fit = None
+            min_size = float('inf')
+            while current: # Iterate over all nodes
+                if current.free >= process.size and current.free < min_size: # if there is enough space and it's smaller than the current best fit
+                    min_size = current.free
+                    best_fit = current
+                current = current.next
+            if best_fit is not None: # if a best fit was found
+                best_fit.free -= process.size
+                process.dir = best_fit.base
+                best_fit.base += process.size
+                processes_exec.append(process) # add to execution list
+                event_counter += 1
+                memory.print_list()
+                make_bar(ram, system_len, processes_exec, memory, method='Mejor ajuste') # plot
+            else: # If no best fit was found
+                processes_waiting.append(process) # add to waiting list
+        
+        while processes_exec:
+            finished_processes = [process for process in processes_exec if process.time == 0] # List of finished processes
+            for process in finished_processes: # for each finished process
+                processes_exec.remove(process) # remove from execution list
+                print(f"----------\nProceso {process.id} terminado.\n----------")
+                # Add a new node to represent the free space
+                self.push_free(process.dir, process.size)
+                self.merge(method='best')
+                event_counter += 1
+                memory.print_list()
+                make_bar(ram, system_len, processes_exec, memory, method='Mejor ajuste')
+
+            for process in processes_exec: # for each process in execution list
+                process.time -= 1 # decrease time
+                print(f"Proceso {process.id} | Tiempo restante: {process.time}") 
+
+            # Check if any waiting processes can be added
+            for waiting_process in processes_waiting[:]: # Iterate over a slice copy of the list
+                current = self.head
+                best_fit = None
+                min_difference = float('inf')
+                while current: # Iterate over all nodes
+                    if current.free >= waiting_process.size and (current.free - waiting_process.size) < min_difference: # if there is enough space and it's smaller than the current best fit
+                        min_difference = current.free - waiting_process.size
+                        best_fit = current
+                    current = current.next
+                if best_fit is not None: # if a best fit was found
+                    best_fit.free -= waiting_process.size
+                    waiting_process.dir = best_fit.base
+                    best_fit.base += waiting_process.size
+                    processes_exec.append(waiting_process) # add to execution list
+                    processes_waiting.remove(waiting_process) # remove from waiting list
+                    event_counter += 1
+                    memory.print_list()
+                    make_bar(ram, system_len, processes_exec, memory, method='Mejor ajuste')
+        print(f"-------\nEventos: {event_counter}")
+        return event_counter
+
+    # worst fit allocation
+    def worst_fit_allocation(self, process_list, ram, system_len, memory):
+        processes_exec = []
+        processes_waiting = []
+        event_counter = 0 
+        process_list_copy = copy.deepcopy(process_list)  # make a copy of the process list
+        for process in process_list_copy: # for each process
+            current = self.head # start at the head
+            worst_fit = None # initialize the worst fit node
+            max_size = 0 # initialize the max size
+            while current: # Iterate over all nodes
+                if current.free >= process.size and current.free > max_size: # if there is enough space and it's bigger than the current worst fit
+                    worst_fit = current # update worst fit
+                    max_size = current.free # update max size
+                current = current.next # move to the next node
+
+            if worst_fit is not None: # if a worst fit was found
+                worst_fit.free -= process.size # allocate the process
+                process.dir = worst_fit.base  # set the base 
+                worst_fit.base += process.size # update the base
+                processes_exec.append(process) # add to execution list
+                event_counter += 1 
+                memory.print_list()
+                make_bar(ram, system_len, processes_exec, memory, method='Worst Fit')
+            else:
+                processes_waiting.append(process)
+
+        while processes_exec:
+            finished_processes = [process for process in processes_exec if process.time == 0] # List of finished processes
+            for process in finished_processes: # for each finished process
+                processes_exec.remove(process) # remove from execution list
+                print(f"----------\nProceso {process.id} terminado.\n----------")
+                # Add a new node to represent the free space
+                self.push_free(process.dir, process.size)
+                self.merge(method='worst')
+                event_counter += 1
+                make_bar(ram, system_len, processes_exec, memory, method='Peor ajuste')
+
+            for process in processes_exec: # for each process in execution list
+                process.time -= 1 # decrease time
+                # print(f"Proceso {process.id} | Tiempo restante: {process.time}") 
+
+            # Check if any waiting processes can be added
+            for waiting_process in processes_waiting[:]: # Iterate over a slice copy of the list
+                current = self.head 
+                worst_fit = None
+                max_size = -1
+                while current: # Iterate over all nodes
+                    if current.free >= waiting_process.size and current.free > max_size: # if there is enough space and it's larger than the current worst fit
+                        max_size = current.free
+                        worst_fit = current
+                    current = current.next
+                if worst_fit is not None: # if a worst fit was found
+                    worst_fit.free -= waiting_process.size
+                    waiting_process.dir = worst_fit.base
+                    worst_fit.base += waiting_process.size
+                    processes_exec.append(waiting_process) # add to execution list
+                    processes_waiting.remove(waiting_process) # remove from waiting list
+                    event_counter += 1
+                    memory.print_list()
+                    make_bar(ram, system_len, processes_exec, memory, method='Peor ajuste')
+        print(f"-------\nEventos: {event_counter}")
+        return event_counter
+
 def make_bar(ram, os_len, processes_exec, mem, method):
     fig, ax = plt.subplots(1, 1, figsize=(5, 7.5), sharey=True)
 
@@ -196,10 +309,18 @@ def make_bar(ram, os_len, processes_exec, mem, method):
 
     plt.show()
 
+def plot_events(first_fit, best_fit, worst_fit):
+    methods = ['Primer ajuste', 'Mejor ajuste', 'Peor ajuste']
+    events = [first_fit, best_fit, worst_fit]
+
+    plt.bar(methods, events)
+    plt.xlabel('Método de asignación')
+    plt.ylabel('Número de eventos')
+    plt.title('Comparación de métodos de asignación')
+    plt.show()
+
 def main():
     procesos = []
-    procesos_exec = []
-    procesos_waiting = []
 
     # DATA
     ram = 2000
@@ -225,7 +346,15 @@ def main():
     memory = LinkedList(ram) #RAM
     memory.push(system_len) #OS
 
-    memory.first_fit_allocation(procesos, procesos_exec, procesos_waiting, ram, system_len, memory)
+    first_fit = memory.first_fit_allocation(procesos, ram, system_len, memory)
+    best_fit = memory.best_fit_allocation(procesos, ram, system_len, memory)
+    worst_fit = memory.worst_fit_allocation(procesos, ram, system_len, memory)
+
+    print(f"Primer ajuste: {first_fit} eventos")
+    print(f"Mejor ajuste: {best_fit} eventos")
+    print(f"Peor ajuste: {worst_fit} eventos")
+
+    plot_events(first_fit, best_fit, worst_fit)
 
 if __name__ == "__main__":
     main()
